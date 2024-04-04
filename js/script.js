c2f7204408762a20620ea3938c31fb8947209e0c
Petr Sahula <pedro@luhas.cz>
2024-04-05 00:23:07 +0200

    Delete unused script

--- js/script.js
+++ js/script.js
@@ -3,9 +3,3 @@ document.addEventListener('DOMContentLoaded', function() {
     var searchButton = document.querySelector('.search-submit');
     searchButton.innerHTML = '<span class="dashicons-before-search"></span>';
 });
-
-if(window.location.href == '/') {
-    document.querySelectorAll('.gallery').forEach(function(element) {
-        element.classList.add('hidden');
-    });
-}
